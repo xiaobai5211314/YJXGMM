5a063701a2ebea2df70a7985398b1b23b1107f76
小白 徐 <1301173082@stu.ahcme.edu.cn>
2023-06-03 16:43:25 +0800

    ok

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,18 +28,148 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            label3 = new Label();
+            label4 = new Label();
             SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("微软雅黑", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            textBox1.Location = new Point(37, 58);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(158, 45);
+            textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("微软雅黑", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            textBox2.Location = new Point(299, 58);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(156, 45);
+            textBox2.TabIndex = 0;
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("微软雅黑", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            textBox3.Location = new Point(37, 226);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(158, 45);
+            textBox3.TabIndex = 0;
+            // 
+            // textBox4
+            // 
+            textBox4.Font = new Font("微软雅黑", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            textBox4.Location = new Point(299, 226);
+            textBox4.Multiline = true;
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(156, 45);
+            textBox4.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("微软雅黑", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(71, 24);
+            label1.Name = "label1";
+            label1.Size = new Size(86, 31);
+            label1.TabIndex = 1;
+            label1.Text = "账户名";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("微软雅黑", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(339, 24);
+            label2.Name = "label2";
+            label2.Size = new Size(62, 31);
+            label2.TabIndex = 1;
+            label2.Text = "密码";
+            // 
+            // button1
+            // 
+            button1.Font = new Font("微软雅黑", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(53, 132);
+            button1.Name = "button1";
+            button1.Size = new Size(142, 48);
+            button1.TabIndex = 2;
+            button1.Text = "修改账户";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("微软雅黑", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.Location = new Point(312, 132);
+            button2.Name = "button2";
+            button2.Size = new Size(130, 48);
+            button2.TabIndex = 2;
+            button2.Text = "修改密码";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("微软雅黑", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(53, 192);
+            label3.Name = "label3";
+            label3.Size = new Size(134, 31);
+            label3.TabIndex = 1;
+            label3.Text = "当前账户名";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("微软雅黑", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.Location = new Point(323, 192);
+            label4.Name = "label4";
+            label4.Size = new Size(110, 31);
+            label4.TabIndex = 1;
+            label4.Text = "当前密码";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(11F, 24F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(634, 450);
+            ClientSize = new Size(482, 282);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private Label label1;
+        private Label label2;
+        private Button button1;
+        private Button button2;
+        private Label label3;
+        private Label label4;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,8 @@
+using System.Diagnostics;
+using System.Management;
+using System.DirectoryServices.AccountManagement;
+
+
 namespace 一键修改账户名密码
 {
     public partial class Form1 : Form
@@ -5,6 +10,89 @@ namespace 一键修改账户名密码
         public Form1()
         {
             InitializeComponent();
+            // 显示当前登录用户名
+            textBox3.Text = Environment.UserName;
+            MessageBox.Show("每修改一次账户名重启一次");
+        }
+
+        // 修改密码
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            string newPassword = textBox2.Text.Trim();
+            try
+            {
+                await Task.Run(() => ChangePassword(newPassword));
+                textBox4.Text = newPassword;
+                MessageBox.Show("密码修改成功！");
+            }
+            catch (Exception ex)
+            {
+                ShowErrorMessage($"密码修改失败：{ex.Message}");
+            }
+        }
+
+        // 修改密码的方法
+        private void ChangePassword(string newPassword)
+        {
+            using (PrincipalContext context = new PrincipalContext(ContextType.Machine))
+            {
+                UserPrincipal user = UserPrincipal.FindByIdentity(context, Environment.UserName);
+                user.SetPassword(newPassword);
+                user.Save();
+            }
+        }
+
+        // 重命名账户
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string oldName = Environment.UserName;
+            string newName = textBox1.Text.Trim();
+
+            if (string.IsNullOrEmpty(newName))
+            {
+                ShowErrorMessage("请输入新的用户名！");
+                return;
+            }
+            try
+            {
+                RenameUserAccount(oldName, newName);
+                textBox3.Text = newName;
+                RestartComputer();
+            }
+            catch (Exception ex)
+            {
+                ShowErrorMessage($"用户名修改失败：{ex.Message}");
+            }
+        }
+
+        // 重命名账户的方法
+        private void RenameUserAccount(string oldName, string newName)
+        {
+            var mo = new ManagementObject($"Win32_UserAccount.Name='{oldName}',Domain='{Environment.UserDomainName}'");
+            var inParams = mo.GetMethodParameters("Rename");
+            inParams["Name"] = newName;
+            var outParams = mo.InvokeMethod("Rename", inParams, null);
+
+            if ((uint)outParams["ReturnValue"] != 0)
+            {
+                throw new Exception("用户名修改失败！");
+            }
+        }
+
+        // 重启电脑的方法
+        private void RestartComputer()
+        {
+            DialogResult dialogResult = MessageBox.Show("是否立即重启电脑？", "重启确认", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                Process.Start("shutdown.exe", "-r -t 0");
+            }
+        }
+
+        // 显示错误信息的方法
+        private void ShowErrorMessage(string message)
+        {
+            MessageBox.Show(message, "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
     }
 }
